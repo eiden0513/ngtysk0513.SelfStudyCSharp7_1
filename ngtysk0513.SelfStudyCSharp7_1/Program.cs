5a015bf784d7d7dfd8791e1e0cb23a7d34da0a9a
eiden0513 <eiden0513@gmail.com>
2018-01-16 22:13:46 +0900

    プロジェクト ファイルを追加します。

---
+++ ngtysk0513.SelfStudyCSharp7_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngtysk0513.SelfStudyCSharp7_1 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
